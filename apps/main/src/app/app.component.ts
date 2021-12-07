4e99b0e6b980cc593e3d0fe252bbec715e23de8b
Pascal Küsgen <pascalkuesgen@gmail.com>
2021-12-07 18:38:37 +0100

    feat: show user how long till a date
    closes #1

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { formatDistanceToNow, parseISO, isValid } from 'date-fns';
+import { de } from 'date-fns/locale'
 
 @Component({
   selector: 'wlnb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'main';
+export class AppComponent implements OnInit {
+  timeUntilInWords = '';
+  targetDateControl = new FormControl(new Date());
+
+  ngOnInit(): void {
+    this.targetDateControl.valueChanges.subscribe(val => {
+      const date = parseISO(val)
+
+      this.timeUntilInWords = isValid(date)
+        ? formatDistanceToNow(date, {locale: de})
+        : ''
+    })
+  }
 }
