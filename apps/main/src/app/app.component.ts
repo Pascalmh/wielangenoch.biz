461c55355c5687a0f57c071aab2878494526433f
Pascal Küsgen <pascalkuesgen@gmail.com>
2022-07-26 21:35:17 +0200

    style: run nx format:write

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import { formatDistanceToNow, parseISO, isValid, isFuture } from 'date-fns';
-import { de } from 'date-fns/locale'
+import { de } from 'date-fns/locale';
 import { combineLatest, debounceTime, startWith } from 'rxjs';
 
 @Component({
@@ -9,32 +9,37 @@ import { combineLatest, debounceTime, startWith } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  timeUntilInWords = ''
-  targetDateControl = new UntypedFormControl(new Date())
-  targetEventNameControl = new UntypedFormControl('')
+  timeUntilInWords = '';
+  targetDateControl = new UntypedFormControl(new Date());
+  targetEventNameControl = new UntypedFormControl('');
 
   ngOnInit(): void {
     combineLatest([
       this.targetDateControl.valueChanges,
-      this.targetEventNameControl.valueChanges.pipe(startWith(''), debounceTime(500))
-    ])
-    .subscribe(([dateAsString, eventName]) => {
-      const date = parseISO(dateAsString)
+      this.targetEventNameControl.valueChanges.pipe(
+        startWith(''),
+        debounceTime(500)
+      ),
+    ]).subscribe(([dateAsString, eventName]) => {
+      const date = parseISO(dateAsString);
 
       if (!isValid(date)) {
-        this.timeUntilInWords = 'Bitte gib ein gültiges Datum ein.'
-        return
+        this.timeUntilInWords = 'Bitte gib ein gültiges Datum ein.';
+        return;
       }
 
       if (!isFuture(date)) {
-        this.timeUntilInWords = 'Das Datum wurde bereits erreicht :-) Das Warten hat eine Ende!'
-        return
+        this.timeUntilInWords =
+          'Das Datum wurde bereits erreicht :-) Das Warten hat eine Ende!';
+        return;
       }
 
-      this.timeUntilInWords = `Es sind noch ${formatDistanceToNow(date, {locale: de})}`
+      this.timeUntilInWords = `Es sind noch ${formatDistanceToNow(date, {
+        locale: de,
+      })}`;
       if (eventName) {
-        this.timeUntilInWords = `${this.timeUntilInWords} bis ${eventName}`
+        this.timeUntilInWords = `${this.timeUntilInWords} bis ${eventName}`;
       }
-    })
+    });
   }
 }
