38a19734df231d404fa023caa9cea98f68c032c2
Pascal Küsgen <pascalkuesgen@gmail.com>
2022-09-02 18:01:08 +0000

    chore: migrate tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   prefix: '',
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      './apps/main/**/*.{html,ts}',
-    ]
-  },
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+     './apps/main/**/*.{html,ts}'
+  ]
 };
