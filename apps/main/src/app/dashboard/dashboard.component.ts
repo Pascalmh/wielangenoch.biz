863bfd49a4fb2441501dcf28f95b1f11cfd7df45
Pascal Küsgen <pascalkuesgen@gmail.com>
2022-09-03 17:24:27 +0000

    feat: add dashboard component

---
+++ apps/main/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AsyncPipe, NgForOf } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'wlnb-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  imports: [
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatGridListModule,
+    AsyncPipe,
+    NgForOf,
+  ],
+})
+export class DashboardComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Card 2', cols: 1, rows: 1 },
+          { title: 'Card 3', cols: 1, rows: 1 },
+          { title: 'Card 4', cols: 1, rows: 1 },
+        ];
+      }
+
+      return [
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 4', cols: 1, rows: 1 },
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,12 +1,27 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter, Route } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
 }
 
+export const ROUTES: Route[] = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./app/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+];
+
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(ROUTES),
+  ],
 });
